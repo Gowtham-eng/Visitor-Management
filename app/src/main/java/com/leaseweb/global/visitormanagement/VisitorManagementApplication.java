efdccb5724396b8f8eff6b82f78e19cfc06130ec
Gowtham-eng <69081075+Gowtham-eng@users.noreply.github.com>
2024-11-27 15:11:46 +0530

    Add files via upload

---
+++ app/src/main/java/com/leaseweb/global/visitormanagement/VisitorManagementApplication.java
@@ -0,0 +1,9 @@
+package com.leaseweb.global.visitormanagement;
+import androidx.multidex.MultiDexApplication;
+
+public class VisitorManagementApplication extends MultiDexApplication {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
